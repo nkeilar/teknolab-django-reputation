63925d129eec85f0938f4289b059f561978fc60a
onur mat <omat@gezgin.com>
2010-05-23 17:56:50 +0300

    cleaned __init__.py

--- reputation/__init__.py
+++ reputation/__init__.py
@@ -1,17 +1,1 @@
 VERSION = (0, 1, None)
-
-from reputation.models import Reputation
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-
-class InitReputation(object):
-    def __init__(self):
-        post_save.connect(self._post_save_signal_callback, sender = User, weak = False)
-    
-    def _post_save_signal_callback(self, **kwargs):
-        instance = kwargs.get('instance', None)
-        created = kwargs.get('created', False)
-        if instance and created:
-            user_reputation = Reputation.objects.reputation_for_user(instance)
-            
-init_reputation = InitReputation()
--- reputation/registry.py
+++ reputation/registry.py
@@ -1,5 +1,20 @@
-from reputation.models import ReputationContent
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from reputation.models import Reputation, ReputationContent
 import reputation.handlers as handlers
+
+
+class InitReputation(object):
+    def __init__(self):
+        post_save.connect(self._post_save_signal_callback, sender = User, weak = False)
+    
+    def _post_save_signal_callback(self, **kwargs):
+        instance = kwargs.get('instance', None)
+        created = kwargs.get('created', False)
+        if instance and created:
+            user_reputation = Reputation.objects.reputation_for_user(instance)
+            
+init_reputation = InitReputation()
 
 
 class ReputationRegistry(object):
